530fcb31c11f7fc173cf0e7ad994e4ea9d0dfb18
tranngocvinh <41569375+tranngocvinh@users.noreply.github.com>
2026-09-01 22:34:22 +0700

    Create music.js

---
+++ music.js
@@ -0,0 +1,37 @@
+/* =========================================================================
+   DANH SÁCH NHẠC NỀN
+   -------------------------------------------------------------------------
+   Thêm bài mới = chép file .mp3 vào thư mục sound/ rồi thêm 1 dòng vào mảng
+   dưới đây. Không cần sửa file nào khác.
+
+   Các field:
+     id      (bắt buộc) khóa duy nhất, đặt gì cũng được miễn không trùng
+     title   (bắt buộc) tên bài hiện trong danh sách
+     artist  ca sĩ, bỏ trống cũng được
+     src     (bắt buộc) đường dẫn tới file, ví dụ "sound/ten-bai.mp3"
+     note    dòng ghi chú nhỏ, ví dụ "bài mình nghe hôm 14/11"
+
+   Bài đầu tiên trong mảng là bài tự phát khi mở sách.
+   ========================================================================= */
+
+const PLAYLIST = [
+    {
+        id: "bai-mac-dinh",
+        title: "Bản nhạc của chúng mình",
+        artist: "",
+        src: "sound/videoplayback.mp3",
+        note: "bài đang dùng làm nhạc nền"
+    },
+
+    // Thêm bài mới theo mẫu này:
+    // {
+    //     id: "em-cua-ngay-hom-qua",
+    //     title: "Em Của Ngày Hôm Qua",
+    //     artist: "Sơn Tùng M-TP",
+    //     src: "sound/em-cua-ngay-hom-qua.mp3",
+    //     note: ""
+    // },
+];
+
+if (typeof window !== 'undefined') window.PLAYLIST = PLAYLIST;
+if (typeof module !== 'undefined') module.exports = PLAYLIST;
